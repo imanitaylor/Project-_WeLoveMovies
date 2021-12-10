e869c0508579f2d17f25ecdbf4dd752d2b083a4f
Imani Taylor <imanir.taylor@gmail.com>
2021-12-10 17:43:33 -0600

    error handlers, no route handler created

--- src/app.js
+++ src/app.js
@@ -2,4 +2,15 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 
+//import the notFound and errorHandler error handlers
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
+
+app.use(express.json());
+
+
+
+app.use(notFound);
+app.use(errorHandler);
+
 module.exports = app;

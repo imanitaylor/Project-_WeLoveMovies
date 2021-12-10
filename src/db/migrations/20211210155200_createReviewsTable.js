5d62d4099602aeaec811df81f06e6b6b5c04e065
Imani Taylor <imanir.taylor@gmail.com>
2021-12-10 16:50:19 -0600

    changed timestamps, to execute in different order

--- src/db/migrations/20211210155200_createReviewsTable.js
+++ src/db/migrations/20211210155200_createReviewsTable.js
@@ -4,8 +4,10 @@ exports.up = function(knex) {
         table.increments("review_id").primary();
         table.text("content");
         table.integer("score");
-        table.foreign("critic_id").references("critic_id").inTable("critics").onDelete("CASCADE");
-        table.foreign("movie_id").references("movie_id").inTable("movies").onDelete("CASCADE");
+        table.integer("critic_id").unsigned().notNullable();
+        table.foreign("critic_id").references("critic_id").inTable("critics").onDelete("cascade");
+        table.integer("movie_id").unsigned().notNullable();
+        table.foreign("movie_id").references("movie_id").inTable("movies").onDelete("cascade");
         table.timestamps(true, true);
     });
       

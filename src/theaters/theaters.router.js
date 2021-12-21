f1e51f9e2d6e0db5a55c70f1b7eb37a5c00ea26c
Imani Taylor <imanir.taylor@gmail.com>
2021-12-20 18:28:21 -0600

    GET all theaters, with nested movies route created

--- src/theaters/theaters.router.js
+++ src/theaters/theaters.router.js
@@ -3,7 +3,7 @@ const controller = require("./theaters.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // - `GET /theaters`
-
+router.route("/").get(controller.list).all(methodNotAllowed);
 
 
 module.exports = router;

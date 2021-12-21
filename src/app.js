b0a2875a38af08572de3ed954deda6449337cd36
Imani Taylor <imanir.taylor@gmail.com>
2021-12-20 18:54:20 -0600

    added cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 //import the notFound and errorHandler error handlers
@@ -11,6 +12,7 @@ const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
